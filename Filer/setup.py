c4ca5daf3dcc529eff2dce35f40b83f18bcebdbc
Demaurr <69788671+Demaurr@users.noreply.github.com>
2024-03-26 14:08:20 +0500

    Version 1.0.2

--- Filer/setup.py
+++ Filer/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Filing',
-    version='1.0.0',
-    packages=find_packages(),
+    name='Filer',
+    version='1.0.2',
+    packages=["Filing"],
     author='Demaurr',
     author_email='',
     description='A package for gathering statistics on files in any given Folder and moving them to a specified(given) folder.',
---
+++ Filer/testing.py
@@ -0,0 +1,1 @@
+from Filer.Filing import filestatser
