133e0cd2a34e890edbfc47c7c0aafa286196b727
Demaurr <69788671+Demaurr@users.noreply.github.com>
2024-07-28 11:23:01 +0500

    Version 1.1.1 Update

--- Filer/setup.py
+++ Filer/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Filer',
-    version='1.1.0',
+    version='1.1.1',
     packages=["Filing"],
     author='Demaurr',
     author_email='',
