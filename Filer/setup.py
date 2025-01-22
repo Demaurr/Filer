ded27c9d61f35fbff029b83e709e71df4e368e78
Demaurr <69788671+Demaurr@users.noreply.github.com.>
2025-01-22 20:13:42 +0500

    Version 1.2.1: Added github url and long_descriotion of documentation

--- Filer/setup.py
+++ Filer/setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("documentation.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='Filer',
-    version='1.1.2',
+    version='1.2.1',
     packages=["Filing"],
     author='Demaurr',
-    author_email='',
-    description='A package for gathering statistics on files in any given Folder and moving them to a specified(given) folder.',
+    author_email='demaurr@example.com',
+    description='A package for gathering statistics on files in any given folder and collectively moving them to any given folder.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/demaurr/filer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[],
+    python_requires='>=3.6',
 )
